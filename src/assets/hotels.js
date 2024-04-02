64395ae27efc0a030556f5bc1f58a932fb0a67e0
John Pascual <ashleypascual@hotmail.es>
2024-04-02 13:31:56 +0100

    import mock api response

---
+++ src/assets/hotels.js
@@ -0,0 +1,63 @@
+export const hotels = [
+    {
+        'id': '1',
+        'name': 'Iberostar Grand Salome',
+        'location': 'Costa Adeje, Tenerife',
+        'rating': 5,
+        'guests': {
+            'adults': 2,
+            'children': 2,
+            'infants': 1
+        },
+        'date': {
+            'date': '',
+            'duration': 7
+        },
+        'departure': 'East Midlands',
+        'price': {
+            'amount': '1,136.50',
+            'currency': '£'
+        },
+        'description': 'This Iberostar Grand Salome has an exceptional location in the south of Tenerife, overlooking the Atlantic Ocean. It is situated between the Golf del Sur and the Amarillo Golf courses, and is an ideal hotel for families, couples and groups who are looking for a holiday full of sport, sun and sea.'
+    },
+    {
+        'id': '2',
+        'name': 'Aguamarina Golf Hotel',
+        'location': 'Costa Adeje, Tenerife',
+        'rating': 4,
+        'guests': {
+            'adults': 2,
+            'children': 1
+        },
+        'date': {
+            'date': '',
+            'duration': 7
+        },
+        'departure': 'Liverpool',
+        'price': {
+            'amount': '696.80',
+            'currency': '£'
+        },
+        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et sollicitudin ac orci phasellus egestas tellus rutrum tellus pellentesque. Dignissim enim sit amet venenatis urna cursus eget nunc scelerisque. Augue ut lectus arcu bibendum at.'
+    },
+    {
+        'id': '3',
+        'name': 'Las Piramides Resort',
+        'location': 'Costa Adeje, Tenerife',
+        'rating': 3,
+        'guests': {
+            'adults': 2,
+            'children': 2
+        },
+        'date': {
+            'date': '',
+            'duration': 7
+        },
+        'departure': 'Manchester',
+        'price': {
+            'amount': '499.99',
+            'currency': '£'
+        },
+        'description': 'Morbi blandit cursus risus at ultrices mi tempus. Malesuada proin libero nunc consequat. In eu mi bibendum neque egestas. Volutpat consequat mauris nunc congue. Sagittis vitae et leo duis ut diam quam nulla porttitor. Tempus quam pellentesque nec nam aliquam sem et tortor.'
+    }
+]
