21b10f7297db20dc3116b37ccbfd0aa0b2808686
John Pascual <ashleypascual@hotmail.es>
2024-04-02 15:49:21 +0100

    add sort component buttons

--- src/components/Sort/Sort.jsx
+++ src/components/Sort/Sort.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Sort = () => {
   return (
-    <div>Sort</div>
+    <div className='sort-container'>
+      <aside>
+        <ul>
+          <li><button>sort <strong>alphabetically</strong></button></li>
+          <li><button>sort by <strong>price</strong></button></li>
+          <li><button>sort by <strong>star rating</strong></button></li>
+        </ul>
+      </aside>
+    </div>
   )
 }
 
